303269c0cf2d0ed7591cda43822a90c322b46a89
Rheymalyn Ann Gregorio <gregoriorheymalyn@gmail.com>
2026-07-31 17:51:37 +0800

    remove spaces

--- script.js
+++ script.js
@@ -30,15 +30,10 @@ function showCategory(category, button) {
 // const questions = document.querySelectorAll(".question");
 
 // questions.forEach(question => {
-
 //     question.addEventListener("click", function () {
-
 //         const answer = this.nextElementSibling;
-
 //         answer.classList.toggle("active");
-
 //     });
-
 // });
 
 const questions = document.querySelectorAll(".question");
