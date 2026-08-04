7feb5b24f5b4fa4358497765e3395c0775b0b1e3
Rheymalyn Ann Gregorio <gregoriorheymalyn@gmail.com>
2026-08-04 16:37:18 +0800

    changes link button

--- script.js
+++ script.js
@@ -107,44 +107,44 @@ questions.forEach(question => {
 //   // more blogs
 // ];
 
-const blogs = [
-    {
-        id: 1,
-        title: "First Blog",
-        date: "2026.08.04",
-        image: "img/blog/blog-img.jpg",
-        detail: "Short description...",
-        content: `
-            <h2>Heading</h2>
-            <p>This is the full article.</p>
-            <img src="img/blog/detail1.jpg" alt="">
-        `
-    },
-    {
-        id: 2,
-        title: "Second Blog",
-        date: "2026.08.05",
-        image: "img/blog/blog-img.jpg",
-        detail: "Another description...",
-        content: `
-            <p>Different article...</p>
-        `
-    }
-];
+// const blogs = [
+//     {
+//         id: 1,
+//         title: "First Blog",
+//         date: "2026.08.04",
+//         image: "img/blog/blog-img.jpg",
+//         detail: "Short description...",
+//         content: `
+//             <h2>Heading</h2>
+//             <p>This is the full article.</p>
+//             <img src="img/blog/detail1.jpg" alt="">
+//         `
+//     },
+//     {
+//         id: 2,
+//         title: "Second Blog",
+//         date: "2026.08.05",
+//         image: "img/blog/blog-img.jpg",
+//         detail: "Another description...",
+//         content: `
+//             <p>Different article...</p>
+//         `
+//     }
+// ];
 
-const cards = document.querySelector(".cards");
+// const cards = document.querySelector(".cards");
 
-blogs.forEach(blog => {
-    cards.innerHTML += `
-        <div class="card">
-            <img src="${blog.image}" alt="">
-            <div class="blog-text">
-                <div class="blog-header">
-                    <h3>${blog.title}</h3>
-                    <p>${blog.date}</p>
-                </div>
-                <p class="blog-detail">${blog.detail}</p>
-            </div>
-        </div>
-    `;
-});
+// blogs.forEach(blog => {
+//     cards.innerHTML += `
+//         <div class="card">
+//             <img src="${blog.image}" alt="">
+//             <div class="blog-text">
+//                 <div class="blog-header">
+//                     <h3>${blog.title}</h3>
+//                     <p>${blog.date}</p>
+//                 </div>
+//                 <p class="blog-detail">${blog.detail}</p>
+//             </div>
+//         </div>
+//     `;
+// });
